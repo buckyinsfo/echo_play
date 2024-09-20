b07a6cba1b14d124f55e0f2ad4efd12742343b4b
buckyinsfo <buckyinsfo@gmail.com>
2024-09-20 11:47:56 -0700

    Remove debug logging

--- components/FiberScene.tsx
+++ components/FiberScene.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, Suspense, useEffect } from 'react';
+import React, { useRef, Suspense } from 'react';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber/native';
 import { TextureLoader, THREE } from 'expo-three';
 import { Asset } from 'expo-asset';
@@ -9,11 +9,6 @@ const Box = () => {
     TextureLoader,
     Asset.fromModule(require('../assets/textures/Ground079L.jpg')).uri,
   );
-
-  useEffect(() => {
-    console.log('Box component mounted');
-    return () => console.log('Box component unmounted');
-  }, []);
 
   useFrame((_, delta) => {
     if (mesh.current) {
@@ -30,36 +25,22 @@ const Box = () => {
   );
 };
 
-const LoadingFallback = () => {
-  useEffect(() => {
-    console.log('LoadingFallback mounted');
-    return () => console.log('LoadingFallback unmounted');
-  }, []);
+const LoadingFallback = () => (
+  <mesh>
+    <boxGeometry args={[1, 1, 1]} />
+    <meshBasicMaterial color="hotpink" />
+  </mesh>
+);
 
-  return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color="hotpink" />
-    </mesh>
-  );
-};
-
-const FiberScene: React.FC = () => {
-  useEffect(() => {
-    console.log('FiberScene mounted');
-    return () => console.log('FiberScene unmounted');
-  }, []);
-
-  return (
-    <Canvas style={{ flex: 1 }}>
-      <color attach="background" args={['lightgreen']} />
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
-      <Suspense fallback={<LoadingFallback />}>
-        <Box />
-      </Suspense>
-    </Canvas>
-  );
-};
+const FiberScene: React.FC = () => (
+  <Canvas style={{ flex: 1 }}>
+    <color attach="background" args={['lightgreen']} />
+    <ambientLight intensity={0.5} />
+    <pointLight position={[10, 10, 10]} />
+    <Suspense fallback={<LoadingFallback />}>
+      <Box />
+    </Suspense>
+  </Canvas>
+);
 
 export default FiberScene;
